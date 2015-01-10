4616bc52ba1623d0298e55ee6e17620555c10164
hacktavist <hacktavist616@gmail.com>
2015-01-09 23:54:17 -0500

    removed some things

--- main.rb
+++ main.rb
@@ -6,8 +6,8 @@ module Dal
 
   class QuotesFromDal < Sinatra::Base
 config = {
-  consumer_key:    'QCgkRhwX85Y2Aj88NER2CrfYS',
-  consumer_secret: '6gEXn5wkKSLlzK2rby1tApCcxulXTIneiUg7FQfKVSTqCuM0LE',
+  consumer_key:    ENV['CONSUMER_KEY'],
+  consumer_secret: ENV['CONSUMER_SECRET'],
 }
 
 client = Twitter::REST::Client.new(config)
@@ -17,8 +17,8 @@ client.user('_hack_ta_vist_')
 puts client.bearer_token.to_s
 
 config = {
-  consumer_key:    'QCgkRhwX85Y2Aj88NER2CrfYS',
-  consumer_secret: '6gEXn5wkKSLlzK2rby1tApCcxulXTIneiUg7FQfKVSTqCuM0LE',
+  consumer_key:    ENV['CONSUMER_KEY'],
+  consumer_secret: ENV['CONSUMER_SECRET'],
   bearer_token: client.bearer_token,
 }
 
